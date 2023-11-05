b08f93ee71c0291fd536d0180a2f198e275a0713
Sergi83 <37980768+Sergi83@users.noreply.github.com>
2023-11-05 20:14:17 +0000

    Deploying to gh-pages from @ Sergi83/goit-advancedjs-hw-01@bd46852b8f996c4e56690fb916e333abb02c693a 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const m=document.querySelector("iframe"),e=new r(m),t="videoplayer-current-time",l=o=>{localStorage.setItem(t,o.seconds)};e.on("timeupdate",a(l,1e3));e.setCurrentTime(localStorage.getItem(t));
+//# sourceMappingURL=commonHelpers2.js.map
